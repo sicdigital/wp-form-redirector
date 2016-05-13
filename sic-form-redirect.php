ba663dd2f9cec3c5faa0b1926e92de0179a6c2de
Mike Chacon <mike@sicdigital.com>
2016-05-13 22:03:19 +0700

    Cleaned up some of the code

--- sic-form-redirect.php
+++ sic-form-redirect.php
@@ -9,19 +9,16 @@
  *
  * @wordpress-plugin
  * Plugin Name: Form Redirector
- * Plugin URI:  https://example.com/plugin-name
  * Description: User enters a password in to a form field and is redirected to a page of your choice.
  * Version:     1.0.0
  * Author:      Your Name
  * Author URI:  https://sicdigital.com
- * Text Domain: plugin-name
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 add_shortcode( 'pw-redirect-field', 'sic_password_field' );
 
-//add_action('admin_post_custom_form_submit','sic_redirect_from_password');
 add_action( 'admin_post_nopriv_sic_pw_submit', 'sic_redirect_from_password' );
 add_action( 'admin_post_sic_pw_submit', 'sic_redirect_from_password' );
 
